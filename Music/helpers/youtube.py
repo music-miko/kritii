35542cf1fbceba1c24f195b11fcb26648ab75f7a
music-miko <voilet856@gmail.com>
2025-11-13 16:27:22 +0530

    Update youtube.py

--- Music/helpers/youtube.py
+++ Music/helpers/youtube.py
@@ -15,38 +15,49 @@ class Hell_YTS:
         encoded_search = urllib.parse.quote_plus(self.search_terms)
         BASE_URL = "https://youtube.com"
         url = f"{BASE_URL}/results?search_query={encoded_search}"
-        response = requests.get(url).text
-        while "ytInitialData" not in response:
-            response = requests.get(url).text
-        results = self._parse_html(response)
-        if self.max_results is not None and len(results) > self.max_results:
-            return results[: self.max_results]
-        return results
 
-    def _parse_html(self, response):
-        results = []
-        start = response.index("ytInitialData") + len("ytInitialData") + 3
-        end = response.index("};", start) + 1
-        json_str = response[start:end]
+        session = requests.Session()
+        response = session.get(url)
+        response.raise_for_status()
+
+        html = response.text
+
+        start = html.find("ytInitialData") + len("ytInitialData") + 3
+        end = html.find("};", start) + 1
+        json_str = html[start:end]
         data = json.loads(json_str)
 
-        videos = data["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"][
-            "sectionListRenderer"
-        ]["contents"][0]["itemSectionRenderer"]["contents"]
+        videos = (
+            data.get("contents", {})
+            .get("twoColumnSearchResultsRenderer", {})
+            .get("primaryContents", {})
+            .get("sectionListRenderer", {})
+            .get("contents", [])[0]
+            .get("itemSectionRenderer", {})
+            .get("contents", [])
+        )
 
+        results = []
         for video in videos:
-            res = {}
-            if "videoRenderer" in video.keys():
-                video_data = video.get("videoRenderer", {})
-                res["id"] = video_data.get("videoId", None)
-                res["thumbnails"] = [
-                    thumb.get("url", None)
-                    for thumb in video_data.get("thumbnail", {}).get("thumbnails", [{}])
-                ]
+            if "videoRenderer" in video:
+                video_data = video["videoRenderer"]
+                res = {}
                 res["title"] = (
-                    video_data.get("title", {}).get("runs", [[{}]])[0].get("text", None)
+                    video_data.get("title", {})
+                    .get("runs", [{}])[0]
+                    .get("text", None)
                 )
-                res["long_desc"] = (
+                res["id"] = video_data.get("videoId", None)
+                res["url"] = (
+                    "https://www.youtube.com/watch?v="
+                    + video_data.get("videoId", None)
+                )
+                res["thumbnail"] = (
+                    video_data.get("thumbnail", {})
+                    .get("thumbnails", [{}])[-1]
+                    .get("url", None)
+                )
+                res["description"] = (
                     video_data.get("descriptionSnippet", {})
                     .get("runs", [{}])[0]
                     .get("text", None)
@@ -56,15 +67,35 @@ class Hell_YTS:
                     .get("runs", [[{}]])[0]
                     .get("text", None)
                 )
-                res["duration"] = video_data.get("lengthText", {}).get("simpleText", 0)
-                res["views"] = video_data.get("viewCountText", {}).get("simpleText", 0)
-                res["publish_time"] = (
-                    lambda d: d.strftime('%d{} of %B, %Y').format(
-                        'th' if 4 <= d.day <= 20 or 24 <= d.day <= 30 else ['st', 'nd', 'rd'][(d.day - 1) % 10 % 3]
-                    )
-                )(
-                    YouTube(("https://www.youtube.com/watch?v=" + video_data.get("videoId", None))).publish_date
+                res["duration"] = video_data.get("lengthText", {}).get(
+                    "simpleText", 0
                 )
+                res["views"] = video_data.get("viewCountText", {}).get(
+                    "simpleText", 0
+                )
+
+                # ---- FIXED PUBLISH TIME HANDLING ----
+                yt_obj = YouTube(
+                    "https://www.youtube.com/watch?v="
+                    + video_data.get("videoId", "")
+                )
+                publish_date = getattr(yt_obj, "publish_date", None)
+                if publish_date:
+                    res["publish_time"] = publish_date.strftime(
+                        "%d{} of %B, %Y"
+                    ).format(
+                        "th"
+                        if 4 <= publish_date.day <= 20
+                        or 24 <= publish_date.day <= 30
+                        else ["st", "nd", "rd"][
+                            (publish_date.day - 1) % 10 % 3
+                        ]
+                    )
+                else:
+                    # fallback to raw text from search results or "Unknown"
+                    res["publish_time"] = video_data.get(
+                        "publishedTimeText", {}
+                    ).get("simpleText", "Unknown")
 
                 res["url_suffix"] = (
                     video_data.get("navigationEndpoint", {})
@@ -73,7 +104,9 @@ class Hell_YTS:
                     .get("url", None)
                 )
                 results.append(res)
-                break
+
+                if self.max_results and len(results) >= self.max_results:
+                    break
         return results
 
     def to_dict(self, clear_cache=True):
