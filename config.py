78e3ba9481db9d44a64e7e16398868592c96674a
music-miko <voilet856@gmail.com>
2025-11-18 05:01:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,13 +22,13 @@ class Config(object):
     BLACK_IMG = getenv("BLACK_IMG", "https://files.catbox.moe/jwc4b6.jpg")        # black image for progress
     BOT_NAME = getenv("BOT_NAME", "Arc Music")   # dont put fancy texts here.
     BOT_PIC = getenv("BOT_PIC", "https://files.catbox.moe/b64xz8.jpg")           # put direct link to image here
-    LEADERBOARD_TIME = getenv("LEADERBOARD_TIME", "4:00")   # time in 24hr format for leaderboard broadcast
+    LEADERBOARD_TIME = getenv("LEADERBOARD_TIME", "8:00")   # time in 24hr format for leaderboard broadcast
     LYRICS_API = getenv("LYRICS_API", None)             # from https://docs.genius.com/
     MAX_FAVORITES = int(getenv("MAX_FAVORITES", 30))    # max number of favorite tracks
     PLAY_LIMIT = int(getenv("PLAY_LIMIT", 0))           # time in minutes. 0 for no limit
     PRIVATE_MODE = getenv("PRIVATE_MODE", "off")        # "on" or "off" to enable/disable private mode
     SONG_LIMIT = int(getenv("SONG_LIMIT", 0))           # time in minutes. 0 for no limit
-    TELEGRAM_IMG = getenv("TELEGRAM_IMG", None)         # put direct link to image here
+    TELEGRAM_IMG = getenv("TELEGRAM_IMG", "https://files.catbox.moe/20hvch.jpg")         # put direct link to image here
     TG_AUDIO_SIZE_LIMIT = int(getenv("TG_AUDIO_SIZE_LIMIT", 104857600))     # size in bytes. 0 for no limit
     TG_VIDEO_SIZE_LIMIT = int(getenv("TG_VIDEO_SIZE_LIMIT", 1073741824))    # size in bytes. 0 for no limit
     TZ = getenv("TZ", "Asia/Kolkata")   # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
